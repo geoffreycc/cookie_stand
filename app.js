1c77bf81275a947eafdc8dfeaccb36007b7feeef
geoffreycc <gcox3d@gmail.com>
2016-04-23 14:02:58 -0700

    Created sales function and added notes from slack.

--- app.js
+++ app.js
@@ -5,12 +5,40 @@
 // Bellevue Square	20	48	3.3
 // Alki	3	24	2.6
 
+
+// Here's an example of rendering to the DOM using nested for loops
+// You will want to consider the structure and apply this to your render method for each object literal today.
+//
+// This is a more advanced for loop inside for loop for nested lists
+// var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// var weeks = [1, 2, 3, 4];
+// var sectionEl = document.getElementById('myList');
+// var ulEl = document.createElement('ul');
+//
+// for(month in months) {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = months[month];
+//   var nestedUlEl = document.createElement('ul');
+//
+//   for(week in weeks) {
+//     var nestedLiEl = document.createElement('li');
+//     nestedLiEl.textContent = weeks[week];
+//     nestedUlEl.appendChild(nestedLiEl);
+//   }
+//
+//   liEl.appendChild(nestedUlEl);
+//   ulEl.appendChild(liEl);
+// }
+//
+// sectionEl.appendChild(ulEl);
+
+
 var pikePlace = {
   min: 17,
   max: 88,
   avg: 5.2,
 }
 
-function getSales(minNu, maxNu){
-  return Math.random * (maxNu - minNu) + minNu; //might need to be return Math.floor(Math.random * (maxNu - minNu)) + minNu;
-};
+function getSales(minNu, maxNu) {
+  return Math.floor(Math.random() * (maxNu - minNu)) + minNu;
+  
