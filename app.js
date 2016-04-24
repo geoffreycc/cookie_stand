3a035d681df02c3c7acfbf4ab8a8939b19efe1da
geoffreycc <gcox3d@gmail.com>
2016-04-23 17:24:52 -0700

    Working to change functions into methods of pikePlace object.

--- app.js
+++ app.js
@@ -38,15 +38,23 @@ var pikePlace = {
   min: 17,
   max: 88,
   avg: 5.2,
+  hourlySales: [],
+  dailyTotal: 0,
   getSales: function (minNum, maxNum) {
     return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
   }
+  // hourSales: function(){
+  //   for (i = 0; i < hours.length; i++) {
+  //     hourlySales[i] = this.getSales(this.min, this.max);
+  //     dailyTotal += hourlySales[i];
+  //   }
+  // }
 }
 
 var dailyTotal = 0;
 var hourlySales = [];
 for (i = 0; i < hours.length; i++) {
-  hourlySales[i] = pikePlace.getSales(pikePlace.min, pikePlace.max);
+  hourlySales[i] = pikePlace.getSales(pikePlace.min, pikePlace.max);         //Add these into the object
   dailyTotal += hourlySales[i];
 }
 
@@ -56,8 +64,7 @@ pikePlace.daily = dailyTotal;
 var elem = document.getElementById('lists');  //grab the main tag in html page
 var ulElem = document.createElement('ul');    // creates an unordered list
 
-// for () {
-//   var liElem = document.createElement( );
-//   liElem.textContent = ;
-//
+// for (hour in hours) {
+//   var liElem = document.createElement();
+//   liElem.textContent = hours[hour];
 // }
