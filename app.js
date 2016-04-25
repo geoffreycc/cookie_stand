1e32c3c2c9d347e86c68f61fd9791ce67cb72b87
geoffreycc <gcox3d@gmail.com>
2016-04-25 11:30:15 -0700

    Added for loop to pikePlace object.

--- app.js
+++ app.js
@@ -4,65 +4,35 @@
 // Southcenter	11	38	1.9
 // Bellevue Square	20	48	3.3
 // Alki	3	24	2.6
-
-
-// Here's an example of rendering to the DOM using nested for loops
-// You will want to consider the structure and apply this to your render method for each object literal today.
-//
-// This is a more advanced for loop inside for loop for nested lists
-// var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-// var weeks = [1, 2, 3, 4];
-// var sectionEl = document.getElementById('myList');
-// var ulEl = document.createElement('ul');
-//
-// for(month in months) {
-//   var liEl = document.createElement('li');
-//   liEl.textContent = months[month];
-//   var nestedUlEl = document.createElement('ul');
-//
-//   for(week in weeks) {
-//     var nestedLiEl = document.createElement('li');
-//     nestedLiEl.textContent = weeks[week];
-//     nestedUlEl.appendChild(nestedLiEl);
-//   }
-//
-//   liEl.appendChild(nestedUlEl);
-//   ulEl.appendChild(liEl);
-// }
-//
-// sectionEl.appendChild(ulEl);
-
-
 var hours = [10, 11, 12, 1, 2, 3, 4, 5, 6];
 var pikePlace = {
   min: 17,
   max: 88,
   avg: 5.2,
+  dayT: 0,
   hourlySales: [],
-  dailyTotal: 0,
   getSales: function (minNum, maxNum) {
     return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlySales += this.getSales(this.min, this.max);
+      this.dayT += this.hourlySales[i];
+    }
   }
-  // hourSales: function(){
-  //   for (i = 0; i < hours.length; i++) {
-  //     hourlySales[i] = this.getSales(this.min, this.max);
-  //     dailyTotal += hourlySales[i];
-  //   }
-  // }
 }
 
-var dailyTotal = 0;
-var hourlySales = [];
-for (i = 0; i < hours.length; i++) {
-  hourlySales[i] = pikePlace.getSales(pikePlace.min, pikePlace.max);         //Add these into the object
-  dailyTotal += hourlySales[i];
-}
-
-pikePlace.hourly = hourlySales;
-pikePlace.daily = dailyTotal;
-
-var elem = document.getElementById('lists');  //grab the main tag in html page
-var ulElem = document.createElement('ul');    // creates an unordered list
+//
+// var dailyTotal = 0;
+// var hourlySales = [];
+// for (i = 0; i < hours.length; i++) {
+//   hourlySales[i] = pikePlace.getSales(pikePlace.min, pikePlace.max);         //Add these into the object
+//   dailyTotal += hourlySales[i];
+// }
+//
+// pikePlace.hourly = hourlySales;
+// pikePlace.daily = dailyTotal;
+//
+// var elem = document.getElementById('lists');  //grab the main tag in html page
+// var ulElem = document.createElement('ul');    // creates an unordered list
 
 // for (hour in hours) {
 //   var liElem = document.createElement();
