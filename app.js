ca1177cac14fc72a5772b2a06d72f1d0d8ab502e
geoffreycc <gcox3d@gmail.com>
2016-04-26 19:18:19 -0700

    Completed seaTac object.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var elm = document.getElementById('lists');
 var hours = ['10am: ', '11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: '];
+var stores = ['pikePlace', 'seaTac', 'SouthCenter', 'bellSquare', 'alki']
 var pikePlace = {
   name: 'Pike Place',
   min: 17,
@@ -8,7 +9,7 @@ var pikePlace = {
   hourly:[],
   daily: 0,
   getSales: function(minNum, maxNum) {
-    return (Math.floor(Math.random() * (maxNum - minNum)) + minNum);
+    return (Math.floor(Math.random() * (maxNum - minNum)) + minNum); //may be able to remove .floor?
   },
   totals: function() {
     var hourlySales = [];
@@ -16,15 +17,13 @@ var pikePlace = {
       hourlySales[s] = Math.floor(this.getSales(this.min, this.max) * this.avg);
       // .push necessary here?
       this.daily += hourlySales[s];
-      console.log(this.daily);
       this.hourly = hourlySales;
-      console.log(this.hourly);
     }
   },
   render: function() {
     var hElm = document.createElement('h1');
     var ulElm = document.createElement('ul');
-    var lisElm = document.createElement('li');  //was lisElm was a p tag
+    var lisElm = document.createElement('li');
     this.totals();
     hElm.textContent = this.name;
     lisElm.textContent = 'Total: ' + this.daily;
@@ -36,25 +35,43 @@ var pikePlace = {
     ulElm.appendChild(lisElm);
     elm.appendChild(hElm);
     elm.appendChild(ulElm);
-    // elm.appendChild(pElm);
   }
 }
-pikePlace.render();
-// var dayTotal = 0;                                                              //Calculates and creates the hourly totals array and the daily total value
-// var hourlySales = [];
-// for (var i = 0; i < hours.length; i++) {
-//   hourlySales[i] = pikePlace.getSales(pikePlace.min, pikePlace.max);
-//   dayTotal += hourlySales[i];
-// }
-// pikePlace.hourly = hourlySales;
-// pikePlace.dayT = dayTotal;
-
 
 var seaTac = {
   name: 'SeaTac Airport',
   min: 6,
   max: 24,
   avg: 1.2,
+  daily: 0,
+  hourly: [],
+  getSales: function(minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum)) + minNum);
+  },
+  totals: function() {
+    var hourlySales = [];
+    for (var s = 0; s < hours.length; s++) {
+      hourlySales[s] = Math.floor(this.getSales(this.min, this.max) * this.avg);
+      this.daily += hourlySales[s];
+      this.hourly = hourlySales;
+    }
+  },
+  render: function() {
+    var hElm = document.createElement('h1');
+    var ulElm = document.createElement('ul');
+    var lisElm = document.createElement('li');
+    this.totals();
+    hElm.textContent = this.name;
+    lisElm.textContent = 'Total: ' + this.daily;
+    for (var r = 0; r < hours.length; r++) {
+      var liElm = document.createElement('li');
+      liElm.textContent = hours[r] + this.hourly[r];
+      ulElm.appendChild(liElm);
+    }
+    ulElm.appendChild(lisElm);
+    elm.appendChild(hElm);
+    elm. appendChild(ulElm);
+  }
 }
 
 var SouthCenter = {
@@ -77,10 +94,6 @@ var alki = {
   max: 24,
   avg: 2.6,
 }
-// var elem = document.getElementById('lists');  //grab the main tag in html page
-// var ulElem = document.createElement('ul');    // creates an unordered list
 
-// for (hour in hours) {
-//   var liElem = document.createElement();
-//   liElem.textContent = hours[hour];
-// }
+pikePlace.render();
+seaTac.render();
