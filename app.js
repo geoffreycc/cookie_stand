9363e7ae72ba21c60b20d22736e9c85f6f8d26dd
geoffreycc <gcox3d@gmail.com>
2016-04-30 10:59:27 -0700

    updated parameter names.

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var elm = document.getElementById('lists');
-var hours = ['10am: ', '11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: '];
+var infoSec = document.getElementById('storeInfo');
+var hoursOpen = ['10am: ', '11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: '];
 var stores = ['pikePlace', 'seaTac', 'SouthCenter', 'bellSquare', 'alki'];
 
-function Store(name, min, max, avg){
-  this.name = name;
-  this.min = min;
-  this.max = max;
-  this.avg = avg;
+function Store(storeName, minCust, maxCust, avgCookie){
+  this.storeName = storeName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookie = avgCookie;
 }
-Store.prototype.hourly = [];
-Store.prototype.daily = 0;
-Store.prototype.getS = function(minNum, maxNum){
+Store.prototype.hourlySales = [];
+Store.prototype.dailyTotal = 0;
+Store.prototype.getSalesNums = function(minNum, maxNum){
   return(Math.random() * (maxNum - minNum)) + minNum;
 };
 Store.prototype.totals = function(rand, maxN, minN, avgC, hour) {
-  var hourlySales = [];
+  var hourlySale = [];
   for(var s = 0; s < hour.length; s++) {
-    hourlySales[s] = Math.floor(rand(minN, maxN) * avgC);
-    this.daily += hourlySales[s];
-    this.hourly = hourlySales;
+    hourlySale[s] = Math.floor(rand(minN, maxN) * avgC);
+    this.dailyTotal += hourlySale[s];
+    this.hourlySales = hourlySale;
   }
 };
 Store.prototype.render = function() {
-  var hElm = document.createElement('h1');
+  var storeNameH1 = document.createElement('h1');
   var ulElm = document.createElement('ul');
   var lisElm = document.createElement('li');
-  this.totals(this.getS, this.max, this.min, this.avg, hours);
-  hElm.textContent = this.name;
-  lisElm.textContent = 'Total: ' + this.daily;
-  for (var r = 0; r < hours.length; r++) {
+  this.totals(this.getSalesNums, this.maxCust, this.minCust, this.avgCookie, hoursOpen);
+  storeNameH1.textContent = this.storeName;
+  lisElm.textContent = 'Total: ' + this.dailyTotal;
+  for (var r = 0; r < hoursOpen.length; r++) {
     var liElm = document.createElement('li');
-    liElm.textContent = hours[r] + this.hourly[r];
+    liElm.textContent = hoursOpen[r] + this.hourlySales[r];
     ulElm.appendChild(liElm);
   }
   ulElm.appendChild(lisElm);
-  elm.appendChild(hElm);
-  elm.appendChild(ulElm);
+  infoSec.appendChild(storeNameH1);
+  infoSec.appendChild(ulElm);
 };
 
 var pikePlace = new Store('Pike Place', 17, 88, 5.2);
