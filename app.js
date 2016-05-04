319b4f20462fdc8038719c4e5dd349d3b6723772
geoffreycc <gcox3d@gmail.com>
2016-05-03 19:53:44 -0700

    Page rendering store name, sales and total.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var infoSec = document.getElementById('storeInfo');
 var hoursOpen = ['10 am: ', '11 am: ','12 pm: ','1 pm: ','2 pm: ','3 pm: ','4 pm: ','5 pm: ','6pm: '];
+var storeArr = [];
 
 function Store(storeName, minCust, maxCust, avgCookie){
   this.storeName = storeName;
@@ -21,21 +22,45 @@ Store.prototype.totals = function(rand, maxN, minN, avgC, hour) {
   }
 };
 Store.prototype.render = function() {
-  var storeNameTr = document.createElement('tr');
-  var ulElm = document.createElement('td');
-  var lisElm = document.createElement('tr');
+  // var storeNameTr = document.createElement('tr'); //tr
+  // var ulElm = document.createElement('td'); //td
+  var lisElm = document.createElement('th'); //tr
   this.totals(this.getSalesNums, this.maxCust, this.minCust, this.avgCookie, hoursOpen);
-  storeNameTr.textContent = this.storeName;
-  ulElm.appendChild(storeNameTr);
-  lisElm.textContent = 'Total: ' + this.dailyTotal;
-  for (var r = 0; r < hoursOpen.length; r++) {
-    var liElm = document.createElement('tr');
-    liElm.textContent = this.hourlySales[r];  //hoursOpen[r] +
-    ulElm.appendChild(liElm);
-  }
-  ulElm.appendChild(lisElm);
-  infoSec.appendChild(ulElm);
+  // storeNameTr.textContent = this.storeName;
+  // infoSec.appendChild(storeNameTr); //ulElm
+  storeArr.push(this);
+  storeTitle(this.storeName, this.hourlySales, this.dailyTotal);
+  // lisElm.textContent = 'Total: ' + this.dailyTotal;
+  // for (var r = 0; r < hoursOpen.length; r++) {
+  //   var liElm = document.createElement('td'); //tr
+  //   liElm.textContent = this.hourlySales[r];  //hoursOpen[r] +
+  //   ulElm.appendChild(liElm);
+  // }
+  // ulElm.appendChild(lisElm);
+  // infoSec.appendChild(ulElm);
 };
+
+// function salesData(data){
+//   for (var d = 0; d < hoursOpen.length; r++) {
+//     var tdElm = document.createElement('td');
+//     tdElm.textContent = data;
+//     storeNameTr.appendChild(tdElm);
+//   }
+// }
+
+function storeTitle(nameS, data, dayTotals){
+  var storeNameTr = document.createElement('tr');
+  var totalTh = document.createElement('th');
+  storeNameTr.textContent = nameS;
+  infoSec.appendChild(storeNameTr);
+  var tdElm = document.createElement('td');
+  for (var d = 0; d < hoursOpen.length; d++) {
+    tdElm.textContent = data;
+    storeNameTr.appendChild(tdElm);
+  }
+  totalTh.textContent = dayTotals;
+  storeNameTr.appendChild(totalTh);
+}
 
 var pikePlace = new Store('Pike Place', 17, 88, 5.2);
 var seaTac = new Store('SeaTac Airport', 6, 24, 1.2);
